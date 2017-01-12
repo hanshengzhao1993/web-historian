4a874e0e9be81dfe709e5bca04ae207192c2123a
hanshengzhao1993 <hanshengzhao1993@gmail.com>
2017-01-11 18:48:27 -0800

    Passed the first test

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -10,10 +10,20 @@ exports.headers = {
   'Content-Type': 'text/html'
 };
 
-exports.serveAssets = function(res, asset, callback) {
+exports.serveAssets = function(response, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+  var file = archive.paths.siteAssets + asset;
+  fs.readFile(file, (err, data) => {
+    if (err) {
+      console.error(err);
+    } 
+    response.write(data);
+    response.end();
+  });
+
+
 };
 
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,26 @@
+var http = require('http');
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var helpers = require('./http-helpers');
+var url = require('url');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+exports.handleRequest = function (request, response) {
+  // var pathName = url.parse( request.url ).pathname;
+  if (request.url === '/') {
+    helpers.serveAssets(response, '/index.html', function () {
+      console.log('end');
+    });
+ 
+  
+  }
+
+  // fs.appendFile( archive.paths.list, 'www.google.com', function (err) {
+  //   if (err) throw err;
+  //   console.log('It\'s saved!');
+  // });
+
+  // console.log(typeof archive.paths, archive.paths);
+  // respond.end( archive.paths.list );
 };
